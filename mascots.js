8d9b0c976cf7b10504da3374041021d3c4804f90
Zanden Koh <137964632+zandenkoh@users.noreply.github.com>
2025-08-31 23:25:14 +0800

    Add files via upload

---
+++ mascots.js
@@ -0,0 +1,33 @@
+// mascots.js
+const mascots = [
+    {
+        name: "Bouncy Cat",
+        image: "https://png.pngtree.com/png-vector/20230831/ourmid/pngtree-cartoon-character-adorable-cat-png-image_9199898.png",
+        type: "normal",
+        cost: 1
+    },
+    {
+        name: "Chill Dog",
+        image: "https://png.pngtree.com/png-vector/20230831/ourmid/pngtree-cartoon-character-adorable-cat-png-image_9199898.png",
+        type: "normal",
+        cost: 2
+    },
+    {
+        name: "Golden Dragon",
+        image: "images/mascots/dragon.png",
+        type: "legendary",
+        cost: 25
+    },
+    {
+        name: "Cosmic Hamster",
+        image: "images/mascots/hamster.png",
+        type: "rare",
+        cost: 10
+    },
+    {
+        name: "Goofy Goose",
+        image: "images/mascots/goose.png",
+        type: "goofy",
+        cost: 5
+    }
+];
