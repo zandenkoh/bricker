3ac8c738567393e4aa982eda3b05bd8f8672f364
Zanden Koh <137964632+zandenkoh@users.noreply.github.com>
2025-09-05 23:38:32 +0800

    Update mascots.js

--- mascots.js
+++ mascots.js
@@ -1,33 +1,72 @@
 // mascots.js
 const mascots = [
-    {
-        name: "Bouncy Cat",
-        image: "https://png.pngtree.com/png-vector/20230831/ourmid/pngtree-cartoon-character-adorable-cat-png-image_9199898.png",
-        type: "normal",
-        cost: 1
-    },
-    {
-        name: "Chill Dog",
-        image: "https://png.pngtree.com/png-vector/20230831/ourmid/pngtree-cartoon-character-adorable-cat-png-image_9199898.png",
-        type: "normal",
-        cost: 2
-    },
-    {
-        name: "Golden Dragon",
-        image: "images/mascots/dragon.png",
-        type: "legendary",
-        cost: 25
-    },
-    {
-        name: "Cosmic Hamster",
-        image: "images/mascots/hamster.png",
-        type: "rare",
-        cost: 10
-    },
-    {
-        name: "Goofy Goose",
-        image: "images/mascots/goose.png",
-        type: "goofy",
-        cost: 5
-    }
+  // Normal
+  {
+    name: "Bouncy Cat",
+    image:
+      "https://png.pngtree.com/png-vector/20230906/ourmid/pngtree-cute-cartoon-cat-png-image_10004760.png",
+    type: "normal",
+    cost: 1,
+  },
+  {
+    name: "Chill Dog",
+    image:
+      "https://png.pngtree.com/png-vector/20230831/ourmid/pngtree-cartoon-character-adorable-cat-png-image_9199898.png",
+    type: "normal",
+    cost: 2,
+  },
+  {
+    name: "Swift Rabbit",
+    image: "https://static.vecteezy.com/system/resources/previews/027/187/565/non_2x/bunny-cute-rabbit-sticker-transparent-free-png.png",
+    type: "normal",
+    cost: 3,
+  },
+
+  // Rare
+  {
+    name: "Cosmic Hamster",
+    image: "https://static.vecteezy.com/system/resources/previews/048/385/098/non_2x/hamster-cartoon-illustration-hamster-on-transparent-background-free-png.png",
+    type: "rare",
+    cost: 10,
+  },
+  {
+    name: "Wise Owl",
+    image: "https://static.vecteezy.com/system/resources/thumbnails/038/885/985/small_2x/ai-generated-3d-cartoon-owl-edc-owl-night-owl-illustration-logo-no-background-perfect-for-print-on-demand-png.png",
+    type: "rare",
+    cost: 12,
+  },
+  {
+    name: "Shadow Fox",
+    image: "https://static.vecteezy.com/system/resources/thumbnails/041/435/822/small_2x/ai-generated-cute-fox-cartoon-illustration-isolated-on-transparent-background-generative-ai-png.png",
+    type: "rare",
+    cost: 15,
+  },
+
+  // Legendary
+  {
+    name: "Golden Dragon",
+    image: "https://static.vecteezy.com/system/resources/thumbnails/018/128/384/small_2x/dragon-zodiac-for-2024-year-3d-rendering-png.png",
+    type: "legendary",
+    cost: 25,
+  },
+  {
+    name: "Phantom Tiger",
+    image: "https://static.vecteezy.com/system/resources/previews/041/645/893/non_2x/ai-generated-tiger-on-transparent-background-image-free-png.png",
+    type: "legendary",
+    cost: 30,
+  },
+  {
+    name: "Celestial Phoenix",
+    image: "https://static.vecteezy.com/system/resources/previews/060/412/025/non_2x/fascinating-victorian-phoenix-fiery-bird-reborn-from-ashes-with-transparent-background-sharp-png.png",
+    type: "legendary",
+    cost: 35,
+  },
+
+  // Goofy (funny / meme-y)
+  {
+    name: "Shrek",
+    image: "https://i.guim.co.uk/img/static/sys-images/Guardian/Pix/pictures/2011/6/20/1308593093645/Shrek-the-musical-007.jpg?width=465&dpr=1&s=none&crop=none",
+    type: "goofy",
+    cost: 5,
+  }
 ];
